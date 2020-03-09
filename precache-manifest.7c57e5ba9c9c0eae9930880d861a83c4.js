97c54be545e679bbb346f957561b1375ac5538a5
Angrey Shoful <shoful.a@gmail.com>
2020-03-09 15:49:52 +0200

    Updates

---
+++ precache-manifest.7c57e5ba9c9c0eae9930880d861a83c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6ad1a1171702c1fa1fc99d75c5ca5a",
+    "url": "/firebase-blog/index.html"
+  },
+  {
+    "revision": "1b6e1919ddcdb361cd05",
+    "url": "/firebase-blog/static/css/main.fd524a1b.chunk.css"
+  },
+  {
+    "revision": "b6a931327dccd06e7fe2",
+    "url": "/firebase-blog/static/js/2.fa1a72c2.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/firebase-blog/static/js/2.fa1a72c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6e1919ddcdb361cd05",
+    "url": "/firebase-blog/static/js/main.ffb56a4b.chunk.js"
+  },
+  {
+    "revision": "507ff75fe82890f9a779",
+    "url": "/firebase-blog/static/js/runtime-main.d6982f3c.js"
+  }
+]);
