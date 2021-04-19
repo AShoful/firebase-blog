0c95b40990e9cda394c4040c0db6e79c6df0091f
Angrey Shoful <shoful.a@gmail.com>
2021-04-19 15:02:09 +0300

    fix NavbarContainer

--- src/Containers/NavbarContainer.js
+++ src/Containers/NavbarContainer.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Navbar from '../Components/Navbar';
 
 import { autoLogin } from '../store/actions/actionsAuth';
 
-const NavbarContainer = ({ authenticated, autoLogin }) => {
-  React.useEffect(() => autoLogin(), [autoLogin]);
+const NavbarContainer = () => {
+  const dispatch = useDispatch();
+  const { authenticated } = useSelector((state) => state.auth);
+  React.useEffect(() => dispatch(autoLogin()), [dispatch]);
   return <Navbar authenticated={authenticated} />;
 };
 
-const mapStateToProps = (state) => ({
-  authenticated: state.auth.authenticated
-});
-const mapDispatchToProps = (dispatch) => ({
-  autoLogin: () => dispatch(autoLogin())
-});
-
-NavbarContainer.propTypes = {
-  authenticated: PropTypes.string,
-  autoLogin: PropTypes.func
-};
-
-NavbarContainer.defaultProps = {
-  authenticated: '',
-  autoLogin: () => {}
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavbarContainer);
+export default NavbarContainer;
